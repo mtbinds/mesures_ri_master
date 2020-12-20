a8ce6de966490896c5aead36c30e730f9cd24b92
Madjid Taoualit <madjid.taoualit@etu.univ-lehavre.fr>
2020-12-20 17:23:49 +0100

    Upload New File

---
+++ src/scenarios/Scenario.java
@@ -0,0 +1,11 @@
+package ri.propagation.scenario;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.graphstream.graph.Node;
+
+public interface Scenario {
+    
+    List<Collection<Node>> propagation(int days);
+}
