b4e52f940863970c19f9d53ae36dcd8b040ed8dd
Madjid Taoualit <madjid.taoualit@etu.univ-lehavre.fr>
2020-12-20 17:29:08 +0100

    Upload New File

---
+++ src/scenarios/Scenario2.java
@@ -0,0 +1,29 @@
+package ri.propagation.scenario;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+public class Scenario2 implements Scenario {
+
+    private SIS sis;
+    private List<Node> nodes;
+
+    public Scenario2(Graph graph) {
+        List<Node> susceptibleNodes = this.nodes = Toolkit.randomNodeSet(graph, (graph.getNodeCount() / 2));
+        this.sis = new SIS(1 / 7D, 1 / 14D, susceptibleNodes);
+    }
+
+    public List<Node> getNodes() {
+        return new ArrayList<>(this.nodes);
+    }
+
+    @Override
+    public List<Collection<Node>> propagation(int days) {
+        return this.sis.propagation(days);
+    }
+}
